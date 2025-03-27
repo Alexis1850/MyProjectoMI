30b9b2825739bb742cf4418499177a7c5266746e
Alexis1850 <alexisacramos@gmail.com>
2025-03-27 17:46:00 +0100

    nuevaModificacion15

--- ArchivoMI.java
+++ ArchivoMI.java
@@ -4,6 +4,7 @@ public static void Principal(sString[] args){
     System.out.pritnln("Texto 1");
     System.out.pritnln("Texto 2");
     System.out.pritnln("Texto 3");
+    System.out.pritnln("Este es mi nuevo syso para mi branch NuevaBranch");
     }
     for (int i=0;i<3;i++){
         System.out.println("Hay al menos "+i" mensaje(s) en el principal")
