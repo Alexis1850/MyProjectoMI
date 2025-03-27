93c3bf472f33c810c59f51ff0fc5858ee4556817
Alexis <alexisacramos@gmail.com>
2025-03-27 17:14:03 +0100

    JEJEJE

--- ArchivoMI.java
+++ ArchivoMI.java
@@ -1,6 +1,11 @@
 public class ArchivoMI{
 
 public static void Principal(sString[] args){
-    System.out.pritnln("Texfefeto MI");
+    System.out.pritnln("Texto 1");
+    System.out.pritnln("Texto 2");
+    System.out.pritnln("Texto 3");
+    }
+    for (int i=0;i<3;i++){
+        System.out.println("Hay al menos "+i" mensaje(s) en el principal")
     }
 }
